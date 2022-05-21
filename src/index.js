ca0dd32d2cf2417426d1a77c4f4c91c800ead56f
vladwwe24 <vladwwe24@gmail.com>
2022-05-22 01:21:47 +0300

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-$("body").mousemove(function(e) {
-  let x = e.pageX - $(this).offset().left - $(this).width() / 6;
-  let y = e.pageY - $(this).offset().top - $(this).height() / 6;
+let bodyDiv = $("body");
+
+bodyDiv.mousemove(function(e) {
+  let x = e.pageX - $(this).offset().left - $(this).width() / 2;
+  let y = e.pageY - $(this).offset().top - $(this).height() / 2;
 
   $("*[data-mouse-parallax]").each(function() {
     let factor = parseFloat($(this).data("mouse-parallax"));
-    x = -x * factor;
-    y = -y * factor;
+    x = -x * factor / 2;
+    y = -y * factor / 2;
 
     $(this).css({ transform: "translate3d( " + x + "px, " + y + "px, 0 )" });
   });
+});
+
+bodyDiv.mouseleave(function(e) {
+    let target = $(e.target);
+
+    if( !target.is("div.layer")) {
+$("*[data-mouse-parallax]").each(function() {
+    $(this).css({ transform: "translate3d( 0px, 0px, 0 )" });
+});
+      e.stopPropagation();
+
+
+    }
 });
